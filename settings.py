b0f58a804c79c913bf83500ee09456c29a18b701
Ceasty <jeffersontimotius@Jeffersons-MacBook-Air.local>
2020-08-22 13:29:10 +0700

    Update

--- settings.py
+++ settings.py
@@ -1,6 +1,16 @@
 
 class Settings:
+
 	def __init__(self):
 		self.window_width = 600
 		self.window_height = 400
 		self.bg_color = (246, 153, 66)
+
+		#ship
+		self.ship_speed = 3
+
+		#bullet
+		self.bullet_speed = 3.0
+		self.bullet_width = 4
+		self.bullet_height = 10
+		self.bullet_color = (60, 242, 19)
